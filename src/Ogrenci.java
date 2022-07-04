df0ef463f5aa1c5eea01f4f253190b63e3bc7864
yener <yenerki@gmail.com>
2022-07-04 21:51:21 +0300

    Öğrenci classı düzeltmeler yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Hello world2");
+        System.out.println("Hello world 3");
     }
 }
